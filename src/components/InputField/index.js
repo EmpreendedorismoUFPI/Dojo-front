13931fe74cec82c428f1a7800ad550a9babba9a7
Lucas Gomes <lucas.g.oliv.dev@gmail.com>
2021-02-12 20:51:16 -0300

    Component input

---
+++ src/components/InputField/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+function InputField() {
+    return (
+        <input id="input-section" type="text"></input>
+    )
+}
+
+export default InputField;
