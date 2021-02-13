18a7afa35f02f226ee8d780413b2edc653bb86bb
Lucas Gomes <lucas.g.oliv.dev@gmail.com>
2021-02-12 21:56:17 -0300

    Adição do component InputField

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
+import InputField from './components/InputField'; 
+
 function App() {
   return (
     <div className="App">
       <h1>Hello Dojô Liga!</h1>
+      <InputField/>
     </div>
   );
 }
